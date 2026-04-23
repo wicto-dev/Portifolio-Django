35cbdd37f1ed107c4efd9ca6a76db50c6a88505c
Wictor <wictordasilva24@gmail.com>
2026-04-23 14:44:08 -0300

    correção de pequenos erros

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("polls/",include("polls.urls")),
-    path('portfolio/',include('portfolio.urls'))
+    path('portfolio/',include('portfolio.urls')),
+    path('', RedirectView.as_view(url='/portfolio/')),
 ]
--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
     
     question_text = models.CharField(max_length=200)
