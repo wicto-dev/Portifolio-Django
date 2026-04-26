f72ff335bfff6e57d9aefb662a66205860d453e1
Wictor <wictordasilva24@gmail.com>
2026-04-26 19:58:49 -0300

    remoção do app polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/",include("polls.urls")),
     path('portfolio/',include('portfolio.urls')),
     path('', RedirectView.as_view(url='/portfolio/')),
 ]
